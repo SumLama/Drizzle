b525a512b42126e685ff7c98a6294228e2bbc920
sumitra tamang <sumi8tamang@gmail.com>
2024-11-14 12:56:34 +0545

    feat: create crud form

---
+++ components/Form.vue
@@ -0,0 +1,214 @@
+<script setup>
+const toast = useToast()
+const { data:userData, status, execute } = await useFetch('/api/user')
+const confirm = ref(false)
+const userToDelete = ref(null)
+const isOpen = ref(false)
+const edit = ref(false)
+const userToEdit = ref(null)
+const user = ref({
+  firstName:'',
+  lastName:'',
+  address:''
+})
+const handleCreate = async ()=>{
+    try{
+    await $fetch('api/user',{
+        method:'POST',
+        body:{
+            firstName:user.value.firstName,
+            lastName : user.value.lastName,
+            address:user.value.address
+        }
+    })
+        toast.add({
+        id: 'created',
+        title: 'Created successfully', 
+        
+     })
+     user.value = {}
+    isOpen.value=false
+    await execute()
+    
+}catch(error){
+    toast.add({
+        id: 'created',
+        title: 'Error',  
+     })
+}
+}
+const confirmDelete = (id) => {
+  userToDelete.value = id
+  confirm.value = true
+}
+
+const cancelDelete = () => {
+
+  confirm.value = false
+  userToDelete.value = null
+}
+
+const deleteUser = async () => {
+  try {
+    await $fetch(`/api/user/${userToDelete.value}`, {
+      method: 'DELETE',
+    })
+    toast.add({
+      title: 'Deleted Successfully',
+    })
+    cancelDelete()
+    await execute() 
+ 
+  } catch (error) {
+    toast.add({
+      title: 'Error',
+      description: 'An error occurred while deleting the user.',
+    
+    })
+  }
+}
+const editUser = (id) => {
+  edit.value = true
+  if (userData.value) {
+     userToEdit.value = userData.value.find((user) => user.id === id)
+    if (userToEdit) {
+      user.value = { ...userToEdit.value }
+    }
+}
+}
+const saveEdit = async()=>{
+  const id = userToEdit.value.id
+  try{
+  await $fetch(`/api/user/${id}`,{
+    method:'PATCH',
+    body:JSON.stringify({
+      ...user.value
+    })
+  })
+  toast.add({
+      title: 'Edited Successfully',
+    })
+    edit.value=false
+    await execute() 
+ 
+}
+catch(err){
+  toast.add({
+    title:"Error",
+    description:'An error occurred while editing the user'
+  })
+}
+}
+const columns = [
+  { key: 'firstName', label: 'First Name' },
+  { key: 'lastName', label: 'Last Name' },
+  { key: 'address', label: 'Address' },
+  { key: 'action', label: 'Action' }
+]
+
+const icons = [
+  { label: 'Edit', icon: 'i-heroicons-pencil-square', color: 'orange' },
+  { label: 'Delete', icon: 'material-symbols-light:delete-outline-sharp', color: 'red' }
+]
+</script>
+<template>
+  <div class="w-3/5 border-2 my-40 mx-auto">
+    <UButton
+      icon="heroicons:plus-16-solid"
+      size="lg"
+      color="blue"
+      variant="solid"
+      label="Add New"
+      class="mt-6 mx-4 px-2 py-2 text-base rounded"
+      :trailing="false"
+       @click="isOpen = true"
+    />
+
+    <div  class="flex justify-center my-10">
+      <UTable :rows="userData" :columns="columns" class="w-full  mx-4 border-2 shadow-lg">
+        <template #action-data="{ row }">
+          <UButton
+            v-for="icon in icons"
+            :key="icon.label"
+            :icon="icon.icon"
+            :color="icon.color"
+            @click="icon.label === 'Delete' ? confirmDelete(row.id) : editUser(row.id)"
+            class="mr-2"
+          />
+        
+        </template>
+      
+      </UTable>
+     
+    </div>
+    <!-- Create user modal -->
+
+    <UModal v-model="isOpen" >
+      <UCard :ui="{ ring: '' }">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-2xl font-bold ">
+                Create User
+            </h3>
+            <UButton color="gray" size="xl" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false" />
+          </div>
+        </template>
+        <form @submit="handleCreate">
+        <label class="font-semibold text-lg">First Name *</label>
+        <UInput v-model="user.firstName" class="my-2" size="lg" placeholder="First Name" required=""/>
+        <label class="font-semibold text-lg">Last Name *</label>
+        <UInput v-model="user.lastName" class="my-2"  size="lg" placeholder="Last Name" required=""/>
+        <label class="font-semibold text-lg">Address *</label>
+        <UInput v-model="user.address" class="my-2"  size="lg" placeholder="Address" required=""/>
+        <div class=" my-4" >
+        <UButton label="Create" color="blue" class="text-base px-5 my-2" type="submit" />
+      </div>
+    </form>
+      </UCard>
+    </UModal>
+    <!-- Confirm modal -->
+     <div>
+      <UModal v-model="confirm" >
+      <UCard :ui="{ ring: '' }">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-lg font-semibold">Are you sure want to delete user?</h3>
+            
+            <UButton color="gray" size="xl" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="cancelDelete" />
+          </div>
+        </template>
+      
+        <UButton label="Confirm" color="blue"  @click="deleteUser" class="mr-3 text-base px-4" />
+         <UButton label="Delete" color="blue" @click="cancelDelete" class="text-base px-4"/>
+      </UCard>
+    </UModal>
+  </div>
+
+  <!-- Edit usermodal -->
+  <UModal v-model="edit" >
+      <UCard :ui="{ ring: '' }">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-2xl font-bold ">
+                Edit User
+            </h3>
+            <UButton color="gray" size="xl" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="edit = false" />
+          </div>
+        </template>
+        <form @submit.prevent="saveEdit" >
+        <label class="font-semibold text-lg">First Name *</label>
+        <UInput v-model="user.firstName" class="my-2" size="lg" placeholder="First Name" required=""/>
+        <label class="font-semibold text-lg">Last Name *</label>
+        <UInput v-model="user.lastName" class="my-2"  size="lg" placeholder="Last Name" required=""/>
+        <label class="font-semibold text-lg">Address *</label>
+        <UInput v-model="user.address" class="my-2"  size="lg" placeholder="Address" required=""/>
+        <div class=" my-4" >
+        <UButton label="Save" color="blue" class="text-base px-5 my-2"  type="submit" />
+      </div>
+    </form>
+      </UCard>
+    </UModal>
+
+    </div>
+  <UNotifications />
+</template>
