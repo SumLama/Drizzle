6cfa36c776239311bd2d0cb27dbed20ef5f5240f
sumitra tamang <sumi8tamang@gmail.com>
2024-11-15 11:25:11 +0545

    chore: add dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from 'nuxt/kit'
+const { resolve } = createResolver(import.meta.url)
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   colorMode:{
     preferences:'light'
+  },
+  runtimeConfig:{
+    dbDir: resolve('./server/db'),
   }
 })
