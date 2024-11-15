eb18bc6229595fc77e2a38fc32c91fd7ce488bc7
sumitra tamang <sumi8tamang@gmail.com>
2024-11-15 11:25:43 +0545

    chore: add add bindings to production

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,10 +1,23 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
- const useDb = ()=>{
-    const dbDir = './server/db'
-    const dbName = 'db.sqlite'
-    const sqlite = new Database(`${dbDir}/${dbName}`)
-    const db = drizzle(sqlite)
-    return db
- }
- export default useDb
+import { drizzle as drizzleD1, DrizzleD1Database } from 'drizzle-orm/d1'
+import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
+import Database from 'better-sqlite3'
+import { join } from 'pathe'
+
+let _db: DrizzleD1Database | BetterSQLite3Database | null = null
+
+export const useDb = () => {
+  if (!_db) {
+    if (process.env.D1) {
+      // Use D1 database in production environment
+      _db = drizzleD1(process.env.D1)
+    } else if (import.meta.dev) {
+      // Use local SQLite database in development environment
+      const { dbDir } = useRuntimeConfig()
+      const sqlite = new Database(join(dbDir, './db.sqlite'))
+      _db = drizzle(sqlite)
+    } else {
+      throw new Error('No database configured for production or development')
+    }
+  }
+  return _db
+}
